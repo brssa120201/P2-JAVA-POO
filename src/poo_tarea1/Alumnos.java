45be52103eeac94fd8d5dce06754f355b0f25465
Brissa Higuita <39968050+brssa120201@users.noreply.github.com>
2020-08-18 21:05:17 -0500

    Trabajo con excepciones

--- src/poo_tarea1/Alumnos.java
+++ src/poo_tarea1/Alumnos.java
@@ -3,14 +3,44 @@ package poo_tarea1;
 //Clase
 public class Alumnos {
 	
-	//Atributos
+	 //Atributos
 	String nombre;
 	
 	//Arreglos
-	Materias[] materias = new Materias[15];
+	static final int MAX_MATERIAS = 8;
+	static final int MAX_CREDITOS = 4;
+	
+	Materias[] materias = new Materias[Alumnos.MAX_MATERIAS];
 	int cantMaterias = 0;
+	Materias[] creditos = new Materias[Alumnos.MAX_CREDITOS];
+	int cantCreditos = 0;
+	
 	
 	//Métodos
+	
+	//Añadir Materias
+	public void AddMaterias(Materias a) throws Exception {
+		if(cantMaterias == MAX_MATERIAS) {
+			throw new Exception("El alumno ya inscribio la cantidad maxima de materias");
+		}
+		
+		materias[cantMaterias] = a;
+		cantMaterias++;
+	}
+	
+	//Añadir Creditos
+	public void AddCreditos(Materias a) throws Exception {
+		for (int s = 0; s<cantCreditos; s++) {
+			if(materias[s].creditos == MAX_CREDITOS) {
+				throw new Exception("El alumno ya no tiene creditos disponibles para inscribir materias");
+			}
+		}
+		
+		materias[cantCreditos] = a;
+		cantCreditos++;
+	}
+	
+	
 	//Promedios Semestre
 	float calPromedio() {
 		float prom = 0;
@@ -48,7 +78,7 @@ public class Alumnos {
 	}
 	
 	
-	//Creditos Perdidos: 
+	//Creditos Perdidos:
 	int calPerCredito() {
 		int credito = 0;
 		for(int l = 0; l<cantMaterias; l++) {
--- src/poo_tarea1/Programa.java
+++ src/poo_tarea1/Programa.java
@@ -1,23 +1,8 @@
 package poo_tarea1;
-/*
- * ENTREGAR: 9 AGOSTO
- * Realizar un programa que realice unos calculos con las notas de unas
- * asignaturas pertenecientes a un estudiante. Cálculos:
- * 1. Promedio del semestre: OK
- * 2. Cuántas notas/asignaturas perdió OK
- * 3. Cuántas asignaturas ganó OK
- * 4. Cada asignatura tiene un número de créditos, se debe indicar cuántos créditos
- * perdió y cuánto dinero debe pagar para repetir los créditos/asignatura. OK * 
- * 5. Crear al menos 5 estudiantes con diferentes valores OK
- * 6. Crear el proyecto en GitHub
- * 
- * ENTREGAR 13 AGOSTO
- * 7. Cambiar el proyecto para que funcione con arreglos OK
- * No permitir asignaturas repetidas OK
- * */
+
 public class Programa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		//Objetos Alumnos
 		Alumnos a1 = new Alumnos();
@@ -31,7 +16,6 @@ public class Programa {
 		Alumnos a5 = new Alumnos();
 		a5.nombre = "Toño";
 		
-		//Alumno 1
 		//Objetos Materias 
 		Materias ma1 = new Materias();
 		ma1.nombre = "Programación";
@@ -53,36 +37,29 @@ public class Programa {
 		
 		Materias ma4 = new Materias();
 		ma4.nombre = "Desarrollo";
-		ma4.notas = 2;
+		ma4.notas = (float) 4.5;
 		ma4.creditos = 4;
 		ma4.valCreditos = 1500;
 		
 		Materias ma5 = new Materias();
 		ma5.nombre = "Electronica";
 		ma5.notas = 2;
-		ma5.creditos = 4;
+		ma5.creditos = 2;
 		ma5.valCreditos = 3700;
 		
-		Materias ma6 = new Materias();
-		ma6.nombre = "Circuitos";
-		ma6.notas = 1;
-		ma6.creditos = 4;
-		ma6.valCreditos = 2800;
-		
 		//Arreglos
-		a1.materias[0] = ma1;
-		a1.cantMaterias++;
-		a1.materias[1] = ma2;
-		a1.cantMaterias++;
-		a1.materias[2] = ma3;
-		a1.cantMaterias++;
-		a1.materias[3] = ma4;
-		a1.cantMaterias++;
-		a1.materias[4] = ma5;
-		a1.cantMaterias++;
-		a1.materias[5] = ma6;
-		a1.cantMaterias++;
-		
+		a1.AddMaterias(ma1);
+		a1.AddCreditos(ma1);
+		a1.AddMaterias(ma2);
+		a1.AddCreditos(ma2);
+		a1.AddMaterias(ma3);
+		a1.AddCreditos(ma3);
+		a1.AddMaterias(ma4);
+		a1.AddCreditos(ma4);
+		a1.AddMaterias(ma5);
+		a1.AddCreditos(ma5);
+								
+						
 		System.out.println("Alumno 1: " + a1.nombre);
 		float promedio = a1.calPromedio();
 		System.out.println("El promedio del alumno es: " + promedio);	
@@ -96,58 +73,50 @@ public class Programa {
 		System.out.println("La cantidad de dinero que debe pagar es: " +dinero);
 		System.out.println("----------------------------------------------------");
 		
-		
 		//Alumno 2
+		Materias ma6 = new Materias();
+		ma6.nombre = "Programación";
+		ma6.notas = 1;
+		ma6.creditos = 2;
+		ma6.valCreditos = 1000;
+		
 		Materias ma7 = new Materias();
-		ma7.nombre = "Programación";
-		ma7.notas = 1;
-		ma7.creditos = 2;
-		ma7.valCreditos = 1000;
+		ma7.nombre = "Ingles";
+		ma7.notas = 2;
+		ma7.creditos = 3;
+		ma7.valCreditos = 5000;
 		
 		Materias ma8 = new Materias();
-		ma8.nombre = "Ingles";
-		ma8.notas = 2;
-		ma8.creditos = 3;
-		ma8.valCreditos = 5000;
+		ma8.nombre = "Matematicas";
+		ma8.notas = 1;
+		ma8.creditos = 1;
+		ma8.valCreditos = 4000;
 		
 		Materias ma9 = new Materias();
-		ma9.nombre = "Matematicas";
-		ma9.notas = 1;
-		ma9.creditos = 1;
-		ma9.valCreditos = 4000;
+		ma9.nombre = "Desarrollo";
+		ma9.notas = 5;
+		ma9.creditos = 2;
+		ma9.valCreditos = 4500;
 		
 		Materias ma10 = new Materias();
-		ma10.nombre = "Desarrollo";
-		ma10.notas = 5;
-		ma10.creditos = 4;
-		ma10.valCreditos = 4500;
-		
-		Materias ma11 = new Materias();
-		ma11.nombre = "Electronica";
-		ma11.notas = 4;
-		ma11.creditos = 4;
-		ma11.valCreditos = 6700;
-		
-		Materias ma12 = new Materias();
-		ma12.nombre = "Circuitos";
-		ma12.notas = 5;
-		ma12.creditos = 4;
-		ma12.valCreditos = 4800;
-		
+		ma10.nombre = "Electronica";
+		ma10.notas = 4;
+		ma10.creditos = 2;
+		ma10.valCreditos = 6700;
+			
 		//Arreglos
-		a2.materias[0] = ma7;
-		a2.cantMaterias++;
-		a2.materias[1] = ma8;
-		a2.cantMaterias++;
-		a2.materias[2] = ma9;
-		a2.cantMaterias++;
-		a2.materias[3] = ma10;
-		a2.cantMaterias++;
-		a2.materias[4] = ma11;
-		a2.cantMaterias++;
-		a2.materias[5] = ma12;
-		a2.cantMaterias++;
-				
+		a2.AddMaterias(ma6);
+		a2.AddCreditos(ma6);
+		a2.AddMaterias(ma7);
+		a2.AddCreditos(ma7);
+		a2.AddMaterias(ma8);
+		a2.AddCreditos(ma8);
+		a2.AddMaterias(ma9);
+		a2.AddCreditos(ma9);
+		a2.AddMaterias(ma10);
+		a2.AddCreditos(ma10);
+		
+					
 		System.out.println("Alumno 2: " + a2.nombre);
 		float promedio1 = a2.calPromedio();
 		System.out.println("El promedio del alumno es: " + promedio1);	
@@ -163,56 +132,50 @@ public class Programa {
 		
 		
 		//Alumno 3:
+		Materias ma11 = new Materias();
+		ma11.nombre = "Programación";
+		ma11.notas = 5;
+		ma11.creditos = 3;
+		ma11.valCreditos = 5000;
+		
+		Materias ma12 = new Materias();
+		ma12.nombre = "Ingles";
+		ma12.notas = 2;
+		ma12.creditos = 3;
+		ma12.valCreditos = 9000;
+		
 		Materias ma13 = new Materias();
-		ma13.nombre = "Programación";
-		ma13.notas = 5;
-		ma13.creditos = 3;
-		ma13.valCreditos = 5000;
+		ma13.nombre = "Matematicas";
+		ma13.notas = 4;
+		ma13.creditos = 2;
+		ma13.valCreditos = 8000;
 		
 		Materias ma14 = new Materias();
-		ma14.nombre = "Ingles";
-		ma14.notas = 2;
-		ma14.creditos = 3;
-		ma14.valCreditos = 9000;
+		ma14.nombre = "Desarrollo";
+		ma14.notas = 1;
+		ma14.creditos = 2;
+		ma14.valCreditos = 2750;
 		
 		Materias ma15 = new Materias();
-		ma15.nombre = "Matematicas";
+		ma15.nombre = "Electronica";
 		ma15.notas = 4;
-		ma15.creditos = 2;
-		ma15.valCreditos = 8000;
+		ma15.creditos = 4;
+		ma15.valCreditos = 8700;
 		
-		Materias ma16 = new Materias();
-		ma16.nombre = "Desarrollo";
-		ma16.notas = 1;
-		ma16.creditos = 2;
-		ma16.valCreditos = 2750;
-		
-		Materias ma17 = new Materias();
-		ma17.nombre = "Electronica";
-		ma17.notas = 4;
-		ma17.creditos = 4;
-		ma17.valCreditos = 8700;
-		
-		Materias ma18 = new Materias();
-		ma18.nombre = "Circuitos";
-		ma18.notas = (float) 1.4;
-		ma18.creditos = 4;
-		ma18.valCreditos = 5600;
-		
+			
 		//Arreglos
-		a3.materias[0] = ma13;
-		a3.cantMaterias++;
-		a3.materias[1] = ma14;
-		a3.cantMaterias++;
-		a3.materias[2] = ma15;
-		a3.cantMaterias++;
-		a3.materias[3] = ma16;
-		a3.cantMaterias++;
-		a3.materias[4] = ma17;
-		a3.cantMaterias++;
-		a3.materias[5] = ma18;
-		a3.cantMaterias++;
+		a3.AddMaterias(ma11);
+		a3.AddCreditos(ma11);
+		a3.AddMaterias(ma12);
+		a3.AddCreditos(ma12);
+		a3.AddMaterias(ma13);
+		a3.AddCreditos(ma13);
+		a3.AddMaterias(ma14);
+		a3.AddCreditos(ma14);
+		a3.AddMaterias(ma15);
+		a3.AddCreditos(ma15);
 		
+				
 		System.out.println("Alumno 3: " + a3.nombre);
 		float promedio2 = a3.calPromedio();
 		System.out.println("El promedio del alumno es: " + promedio2);	
@@ -228,56 +191,49 @@ public class Programa {
 		
 		
 		//Alumno 4:
+		Materias ma16 = new Materias();
+		ma16.nombre = "Programación";
+		ma16.notas = 5;
+		ma16.creditos = 2;
+		ma16.valCreditos = 15000;
+		
+		Materias ma17 = new Materias();
+		ma17.nombre = "Ingles";
+		ma17.notas = (float) 2.5;
+		ma17.creditos = 2;
+		ma17.valCreditos = 9000;
+		
+		Materias ma18 = new Materias();
+		ma18.nombre = "Matematicas";
+		ma18.notas = 2;
+		ma18.creditos = 2;
+		ma18.valCreditos = 2000;
+		
 		Materias ma19 = new Materias();
-		ma19.nombre = "Programación";
-		ma19.notas = 5;
-		ma19.creditos = 2;
-		ma19.valCreditos = 15000;
+		ma19.nombre = "Desarrollo";
+		ma19.notas = 2;
+		ma19.creditos = 3;
+		ma10.valCreditos = 750;
 		
 		Materias ma20 = new Materias();
-		ma20.nombre = "Ingles";
-		ma20.notas = (float) 2.5;
-		ma20.creditos = 2;
-		ma20.valCreditos = 9000;
-		
-		Materias ma21 = new Materias();
-		ma21.nombre = "Matematicas";
-		ma21.notas = 2;
-		ma21.creditos = 2;
-		ma21.valCreditos = 2000;
-		
-		Materias ma22 = new Materias();
-		ma22.nombre = "Desarrollo";
-		ma22.notas = 2;
-		ma22.creditos = 3;
-		ma22.valCreditos = 750;
-		
-		Materias ma23 = new Materias();
-		ma23.nombre = "Electronica";
-		ma23.notas = 1;
-		ma23.creditos = 6;
-		ma23.valCreditos = 700;
-		
-		Materias ma24 = new Materias();
-		ma24.nombre = "Circuitos";
-		ma24.notas = 5;
-		ma24.creditos = 2;
-		ma24.valCreditos = 500;
+		ma20.nombre = "Electronica";
+		ma20.notas = 1;
+		ma20.creditos = 6;
+		ma20.valCreditos = 700;
 		
 		//Arreglos
-		a4.materias[0] = ma19;
-		a4.cantMaterias++;
-		a4.materias[1] = ma20;
-		a4.cantMaterias++;
-		a4.materias[2] = ma21;
-		a4.cantMaterias++;
-		a4.materias[3] = ma22;
-		a4.cantMaterias++;
-		a4.materias[4] = ma23;
-		a4.cantMaterias++;
-		a4.materias[5] = ma24;
-		a4.cantMaterias++;
-	
+		a4.AddMaterias(ma16);
+		a4.AddCreditos(ma16);
+		a4.AddMaterias(ma17);
+		a4.AddCreditos(ma17);
+		a4.AddMaterias(ma18);
+		a4.AddCreditos(ma18);
+		a4.AddMaterias(ma19);
+		a4.AddCreditos(ma19);
+		a4.AddMaterias(ma20);
+		a4.AddCreditos(ma20);
+		
+			
 		System.out.println("Alumno 4: " + a4.nombre);
 		float promedio3 = a4.calPromedio();
 		System.out.println("El promedio del alumno es: " + promedio3);	
@@ -293,56 +249,49 @@ public class Programa {
 		
 		
 		//Alumno 5:
+		Materias ma21 = new Materias();
+		ma21.nombre = "Programación";
+		ma21.notas = 2;
+		ma21.creditos = 3;
+		ma21.valCreditos = 10000;
+		
+		Materias ma22 = new Materias();
+		ma22.nombre = "Ingles";
+		ma22.notas = 4;
+		ma22.creditos = 3;
+		ma22.valCreditos = 9000;
+		
+		Materias ma23 = new Materias();
+		ma23.nombre = "Matematicas";
+		ma23.notas = 1;
+		ma23.creditos = 2;
+		ma23.valCreditos = 5000;
+		
+		Materias ma24 = new Materias();
+		ma24.nombre = "Desarrollo";
+		ma24.notas = 2;
+		ma24.creditos = 2;
+		ma24.valCreditos = 2350;
+		
 		Materias ma25 = new Materias();
-		ma25.nombre = "Programación";
-		ma25.notas = 2;
-		ma25.creditos = 3;
-		ma25.valCreditos = 10000;
-		
-		Materias ma26 = new Materias();
-		ma26.nombre = "Ingles";
-		ma26.notas = 4;
-		ma26.creditos = 3;
-		ma26.valCreditos = 9000;
-		
-		Materias ma27 = new Materias();
-		ma27.nombre = "Matematicas";
-		ma27.notas = 1;
-		ma27.creditos = 2;
-		ma27.valCreditos = 5000;
-		
-		Materias ma28 = new Materias();
-		ma28.nombre = "Desarrollo";
-		ma28.notas = 2;
-		ma28.creditos = 2;
-		ma28.valCreditos = 2350;
-		
-		Materias ma29 = new Materias();
-		ma29.nombre = "Electronica";
-		ma29.notas = 5;
-		ma29.creditos = 2;
-		ma29.valCreditos = 6700;
-		
-		Materias ma30 = new Materias();
-		ma30.nombre = "Circuitos";
-		ma30.notas = 2;
-		ma30.creditos = 3;
-		ma30.valCreditos = 900;
+		ma25.nombre = "Electronica";
+		ma25.notas = 5;
+		ma25.creditos = 10;
+		ma25.valCreditos = 6700;
 		
 		//Arreglos
-		a5.materias[0] = ma25;
-		a5.cantMaterias++;
-		a5.materias[1] = ma26;
-		a5.cantMaterias++;
-		a5.materias[2] = ma27;
-		a5.cantMaterias++;
-		a5.materias[3] = ma28;
-		a5.cantMaterias++;
-		a5.materias[4] = ma29;
-		a5.cantMaterias++;
-		a5.materias[5] = ma30;
-		a5.cantMaterias++;
-	
+		a5.AddMaterias(ma21);
+		a5.AddCreditos(ma21);
+		a5.AddMaterias(ma22);
+		a5.AddCreditos(ma22);
+		a5.AddMaterias(ma23);
+		a5.AddCreditos(ma23);
+		a5.AddMaterias(ma24);
+		a5.AddCreditos(ma24);
+		a5.AddMaterias(ma25);
+		a5.AddCreditos(ma25);
+
+		
 		System.out.println("Alumno 5: " + a5.nombre);
 		float promedio4 = a5.calPromedio();
 		System.out.println("El promedio del alumno es: " + promedio4);	
@@ -355,6 +304,7 @@ public class Programa {
 		float dinero4 = a5.calDinero();
 		System.out.println("La cantidad de dinero que debe pagar es: " +dinero4);
 		System.out.println("----------------------------------------------------");
-		
+	
 }
+		
 }
